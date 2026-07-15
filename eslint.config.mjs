2e86c7ac02a411a1860d92556a041db8dd937e44
JRJ24 <jonaifryrodriguezdejesus5@gmail.com>
2026-07-15 12:32:32 -0400

    feat: add files middlewares + eslint modifed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,30 +6,59 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'dist/**',
+      'coverage/**',
+      'node_modules/**',
+      'src/generated/**',
+      'generated/**',
+    ],
   },
+
   eslint.configs.recommended,
+
   ...tseslint.configs.recommendedTypeChecked,
+
   eslintPluginPrettierRecommended,
+
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+
+      sourceType: 'module',
+
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
+
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+
       '@typescript-eslint/no-floating-promises': 'warn',
+
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+
+      '@typescript-eslint/no-empty-object-type': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+
+      '@typescript-eslint/prefer-promise-reject-errors': 'error',
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 );
